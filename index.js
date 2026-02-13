8e6d4552a9833f360dd0b8f14e91eec766c927dd
meiad-khan <meiadkhan615@gmail.com>
2026-02-13 16:43:36 +0600

    added get api for latest books

--- index.js
+++ index.js
@@ -58,6 +58,12 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/latest-books', async (req, res) => {
+      const query = { bookStatus: 'Published' };
+      const result = await bookCollection.find(query).sort({ createdAt: -1 }).limit(6).toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
