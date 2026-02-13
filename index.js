812fbdec3f94cf2acff7187f4ae499338d4845d2
meiad-khan <meiadkhan615@gmail.com>
2026-02-13 15:24:08 +0600

    added post api for user and books

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-//
-//
 
 const uri =
   `${process.env.URI}`;
@@ -26,6 +29,35 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db('BookCourier_DB');
+    const userCollection = db.collection('users');
+    const bookCollection = db.collection('books');
+
+    //user related api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      user.role = "user";
+      user.createdAt = new Date();
+      const email = user.email;
+      const userExist = await userCollection.findOne({ email });
+      if (userExist) {
+        return res.send({ message: "User already exist" });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+
+    //book related api
+    app.post('/books', async (req, res) => {
+      const newBook = req.body;
+      newBook.createdAt = new Date();
+      const result = await bookCollection.insertOne(newBook);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
